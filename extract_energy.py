170296365ea596b3fcb1912e709d9e015ee86a24
Logan Ritter <32370525+loganritter@users.noreply.github.com>
2025-02-04 17:30:53 -0500

    Update extract_energy.py
    added sys.argv

--- extract_energy.py
+++ extract_energy.py
@@ -1,16 +1,17 @@
-# String search and replace
+import sys
+
 configs_xxx = "XXX"
 
-with open(r'ne.dat', 'r') as file:
+with open(sys.argv[1], 'r') as file:
     energy = []
     for line in file:
         a = line.split()
-        energy.append(str(a[1]))
+        energy.append(str(a[0]))
 
-with open(r'configs.fit.fixed', 'r') as file:
-        data = file.read()
-        for i in range(len(energy)):
-            data = data.replace(configs_xxx, energy[i], 1)
+with open(r'configs.fit', 'r') as file:
+    data = file.read()
+    for i in range(len(energy)):
+        data = data.replace(configs_xxx, energy[i], 1)
 
 with open(r'configs.fit.fixed', 'w') as file:
     file.write(data)
